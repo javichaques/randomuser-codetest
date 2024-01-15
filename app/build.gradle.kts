fe7eaaec41d42b30eaa9973c66b49144b2f7a5a8
Javi Chaqués <javichaques@gmail.com>
2024-01-15 22:29:05 +0100

    chore: add format check

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,13 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.junit)
+    alias(libs.plugins.hilt)
+//    alias(libs.plugins.google.services)
+//    alias(libs.plugins.firebase.crashlytics)
+//    alias(libs.plugins.firebase.appdistribution)
 }
 
 android {
@@ -13,8 +20,6 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
@@ -23,21 +28,55 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
     }
 }
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
+    implementation(libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    ksp(libs.hilt.compiler)
+
+    implementation(platform(libs.compose.bom))
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.core)
+    implementation(libs.androidx.splashscreen)
+    implementation(libs.androidx.startup)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.destinations.core)
+    ksp(libs.destinations.ksp)
+
+    implementation(libs.timber)
+
+//    implementation(platform(libs.firebase.bom))
+//    implementation(libs.firebase.analytics)
+//    implementation(libs.firebase.crashlytics)
+
+//    implementation(platform(libs.arrow.bom))
+//    implementation(libs.arrow.core)
+//    implementation(libs.arrow.retrofit)
+
+//    implementation(libs.coil.core)
+
+    debugImplementation(libs.leakcanary)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,56 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
+    alias(libs.plugins.updates)
+
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.junit) apply false
+    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.hilt) apply false
+}
+
+allprojects {
+    // Spotless
+    apply(plugin = rootProject.libs.plugins.spotless.get().pluginId)
+
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            targetExclude("${layout.buildDirectory}/**/*.kt")
+            targetExclude("bin/**/*.kt")
+
+            ktlint().setEditorConfigPath("$rootDir/.editorconfig")
+        }
+        kotlinGradle {
+            target("**/*.kts")
+            targetExclude("${layout.buildDirectory}/**/*.kts")
+
+            ktlint().setEditorConfigPath("$rootDir/.editorconfig")
+        }
+        format("misc") {
+            target("**/*.md", "**/.gitignore")
+        }
+        format("xml") {
+            target("**/*.xml")
+        }
+    }
+}
+
+// Gradle Versions
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
